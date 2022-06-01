66fea96b8157c3c2b9862a687c815b174ad67ceb
ashkanfekridev <ashkanfekridev@gmail.com>
2022-06-01 12:48:24 +0430

    update product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * @method static create(array $array)
+ * @method static findOrFail(int $id)
+ */
 class Product extends Model
 {
     use HasFactory;
@@ -18,4 +23,15 @@ class Product extends Model
         'category_id',
         'image_id'
     ];
+
+    public function image()
+    {
+        return $this->hasOne(Image::class, 'id', 'image_id');
+    }
+
+    public function category()
+    {
+        return $this->hasOne(Category::class, 'id', 'category_id');
+    }
+
 }
