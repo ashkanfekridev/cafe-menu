376a4e19079b924e44368b585713c14c62d4c230
ashkanfekridev <ashkanfekridev@gmail.com>
2022-06-01 12:48:06 +0430

    update HomeController.php

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -11,10 +13,17 @@ class HomeController extends Controller
     public function __invoke(Request $request)
     {
         $productAndCategoryCount = DB::select("select
-        (select count(*) from categories WHERE user_id = ?) as category,
-        (select count(*) from products WHERE user_id=?) as product",[1,1]
+        (select count(*) from categories WHERE user_id = ?) as categoryCount,
+        (select count(*) from products WHERE user_id=?) as productCount", [1, 1]
         );
-//        return $productAndCategoryCount;
-        return view('admin.home');
+
+        $userWithCategoriesAndProduct = User::with(['categories', 'products'])->first();
+
+        $productAndCategoryCount = $productAndCategoryCount[0];
+
+        return view('admin.home', [
+            'productAndCategoryCount' => $productAndCategoryCount,
+            'userWithCategoriesAndProduct' => $userWithCategoriesAndProduct
+        ]);
     }
 }
