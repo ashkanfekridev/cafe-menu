f2e1ea0879d36bb123a2ba69a30f0efba1052ed1
ashkanfekridev <ashkanfekridev@gmail.com>
2022-06-13 17:49:35 +0430

    fix HomeController.php

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -7,18 +7,25 @@ use App\Models\Product;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
 
 class HomeController extends Controller
 {
     public function __invoke(Request $request)
     {
+      
+        $user = Auth::user();
         $productAndCategoryCount = DB::select("select
         (select count(*) from categories WHERE user_id = ?) as categoryCount,
-        (select count(*) from products WHERE user_id=?) as productCount", [1, 1]
+        (select count(*) from products WHERE user_id=?) as productCount", [ $user->id,   $user->id]
         );
 
-        $userWithCategoriesAndProduct = User::with(['categories', 'products'])->first();
 
+
+        $userWithCategoriesAndProduct = new \stdClass();
+        $userWithCategoriesAndProduct->products = User::find($user->id)->products;
+        $userWithCategoriesAndProduct->categories = User::find($user->id)->categories;
         $productAndCategoryCount = $productAndCategoryCount[0];
 
         return view('admin.home', [
